a484e1b8fbb44ea5734c906c8b285d72258f76d6
Lokesh Varman <30750553+LokeshVarman@users.noreply.github.com>
2019-08-27 11:29:29 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,31 @@
+#starter
+print ("I am a techgeek")
+lokesh="Techgeek"
+print(lokesh)
+msg='adf'
+msg=56
+print(msg)
+#implicit
+i=10
+j=20.5
+type(j)
+k=20.5//10
+print(k)
+print(type(k))
+#concat
+str1="kirly"
+str2="boy"
+str3= 5 
+str4=str1 + str(str3)
+print(str4)
+print(str1 * 5)
+#listcomprehension
+lokeshfav={x for x in range(1,10) if(x==5)}
+print(lokeshfav)
+#listslicing
+loki="lokeshvarman"
+   #loki=loki[:3]
+   #print(loki)
+print(type(loki))
+loki=loki[::-1]
+print(loki)
