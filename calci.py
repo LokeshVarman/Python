0784637ee5f792ac1a627a654b4a9bf5e908ceb4
Lokesh Varman <30750553+LokeshVarman@users.noreply.github.com>
2019-11-20 06:44:04 +0530

    Update calci.py

--- calci.py
+++ calci.py
@@ -5,13 +5,18 @@ print("1.add \n 2.subtract \n 3.divide \n 4.multiply \n 5.modulo" )
 choice=input("enter your choice")
 if choice=="1":
   ans=a+b
+  print(ans)
 if choice=="2":
   ans=a-b
+  print(ans)
 if choice=="3":
   ans=a/b
+  print(ans)
 if choice=="4":
   ans=a*b
+  print(ans)
 if choice=="5":
   ans=a%b
+  print(ans)
 else:
   print("Please enter the choice given")
